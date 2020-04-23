a8c6ade793fac81c5ac8b45065c52a760d5cb56c
加賀谷愛美 <manami@kagayamanaminoMacBook-Pro.local>
2020-04-24 00:50:41 +0900

    setting cp

--- js/js-practice.js
+++ js/js-practice.js
@@ -1,4 +1,15 @@
-document.write("Hello World!!");
+document.write("Hello World!");
 // alert("Hello World!");
 
-console.log("Hello World!" + "加賀谷さん");
+console.log("Hello World!" + "加賀谷さん")
+
+console.log(10 + 10)
+console.log("10" + "10")
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(2 * 2);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
